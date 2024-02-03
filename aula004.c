79a2a02e3e96317c89c50b0f4e4598343adba055
Dulcio Bernardo <140249019+Dulciobernard@users.noreply.github.com>
2024-02-03 13:46:05 +0100

    aula004

---
+++ aula004.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define texto"Entrada e saida de dados"
+int main(){
+ printf("%s\n",texto);
+ int idade = 0;
+ float altura =0.0;
+ char nome[50]= "";
+
+ printf("\nDigite a sua idade:");
+ scanf("%d",&idade);
+
+  printf("\nDigite a sua altura:");
+ scanf("%f",&altura);
+  printf("\nDigite o seu nome:");
+ scanf("%s",&nome);
+
+ printf("O %s tem %d idade e %.2f de altura",nome,idade,altura);
+}
