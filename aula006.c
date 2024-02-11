36e5febfbda5f3af855329c9cbab9e26b937a367
Dulcio Bernardo <140249019+Dulciobernard@users.noreply.github.com>
2024-02-11 19:37:45 +0100

    atual

--- aula006.c
+++ aula006.c
@@ -3,6 +3,28 @@
 int main()
 {
     int n1, n2, maior, menor;
-    printf("Digite um numero");
-    scanf("%d", &n1)
-}
+    int dif;
+    printf("Digite um numero:");
+    scanf("%d",&n1);
+    printf("\nDigite outro numero:");
+    scanf("%d",&n2);
+     dif=n1-n2;
+    if (n1>n2)
+    {
+        maior=n1;
+        menor=n2;
+    }else{
+        menor=n1;
+        maior=n2;
+    }
+    printf("Maior:%d\n",maior);
+    printf("Menor:%d\n",menor);
+    printf("A direnca entre %d e %d e:%d",n1,n2,dif);
+    
+
+    
+
+    
+    
+    
+    }
