91d797301f53398eadc7168e842c0f5b5d99517e
Dulcio Bernardo <140249019+Dulciobernard@users.noreply.github.com>
2024-02-11 14:27:24 +0100

    aula006

--- aula006.c
+++ aula006.c
@@ -1,8 +1,8 @@
-#include<stdio.h>
+#include <stdio.h>
 
-
-int main() {
-    int n1,n2,maior ,menor;
-     printf("Digite um numero");
-     scanf("%d",&n1);
+int main()
+{
+    int n1, n2, maior, menor;
+    printf("Digite um numero");
+    scanf("%d", &n1)
 }
