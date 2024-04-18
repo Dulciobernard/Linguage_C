91f06a899dcbb28d2171bc96bc50505ac0ed8da6
Dulcio Bernardo <140249019+Dulciobernard@users.noreply.github.com>
2024-04-18 20:01:33 +0100

    save

---
+++ ExercíciosdeAlgoritmos/exercicios026.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*26) Escreva um algoritmo que leia dois números inteiros e compare-os, mostrando 
+na tela uma das mensagens abaixo: 
+- O primeiro valor é o maior 
+-O segundo valor é o maior 
+ - Não existe valor maior, os dois são iguais*/
+int main(){
+   int n1,n2,maior,menor;
+   printf("digite o primeiro numero:");
+   scanf("%d",&n1);
+   printf("digite o segundo numero:");
+   scanf("%d",&n2);
+}
---
+++ ExercíciosdeAlgoritmos/exercicios027.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/*27) Crie um programa que leia duas notas de um aluno e calcule a sua média, 
+mostrando uma mensagem no final, de acordo com a média atingida: 
+- Média até 4.9: REPROVADO 
+- Média entre 5.0 e 6.9: RECUPERAÇÃO 
+- Média 7.0 ou superior: APROVADO */
+int main(){
+   float nota1,nota2,media;
+   printf("digite o primeiro numero:");
+   scanf("%f",&nota1);
+   printf("digite o segundo numero:");
+   scanf("%f",&nota2);
+
+   media=(nota1+nota2)/2;
+
+   if (media < 4.9)
+   {
+    printf("O aluno foi reprovado com a media :%f",media);
+   }if else(media >= 5.0 && media < 6.9){
+      printf("O aluno foi ao recurso com a media :%f",media);
+   }else{
+      printf("O aluno Aprovado com a media :%f",media);
+   }
+   
+}
